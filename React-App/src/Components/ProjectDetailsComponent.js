1e13a48fe6ef70c4aef05638e41c9604bbde598e
Tushar Agrawal <tusharagrawal16@rediffmail.com>
2021-04-08 14:28:43 +0530

    update ui to generate token

--- React-App/src/Components/ProjectDetailsComponent.js
+++ React-App/src/Components/ProjectDetailsComponent.js
@@ -107,17 +107,29 @@ const ProjectDetails = ({
         </div>
       </div>
       {data.accessToken && showAccessCode() ? (
-        <div className="mx-2 my-2 text-">Access Token: <span className="text-lg bg-blue-200 px-2 text-blue-700 italic border-blue-600 rounded">{data.accessToken}</span></div>
+        <div className="mx-2 my-2 flex flex-col">
+          <span>
+            Access Token:{" "}
+            <span className="text-lg bg-blue-200 px-2 text-blue-700 italic border-blue-600 rounded">
+              {data.accessToken}
+            </span>
+          </span>{" "}
+          <span>Valid Till: 
+          <span className="text-sm bg-yellow-200 px-2 text-yellow-700 italic mx-2 border-yellow-600 border-2 rounded"> {data.tokenValidity.toDate().toLocaleString()}
+          
+          </span></span>
+        </div>
       ) : (
-        <div>
+        <div className="flex flex-col items-start">
           <button
-            className={`bg-blue-200 text-blue-700 border-2 border-blue-600 text-base px-2 rounded mx-2 my-2 ${
+            className={`bg-blue-200 text-blue-700 border-2 border-blue-600 text-base px-2 rounded mx-2 my-1 ${
               isTaskLoading ? "cursor-not-allowed opacity-50" : null
             }`}
             onClick={handleGenerateToken}
           >
             Generate Token
           </button>
+          <span className="bg-yellow-200 px-2 text-yellow-700 italic border-yellow-600 border-2 mx-2 rounded text-sm">Valid for 15mins</span>
         </div>
       )}
 
